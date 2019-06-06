ad2eb98679e09f2b182f838c27699a85138b36e1
antuan159 <lanos159@ukr.net>
2019-06-06 17:17:07 +0300

    first try

---
+++ module-03/js/app.js
@@ -0,0 +1,27 @@
+const logins = ['Mango', 'robotGoogles', 'Poly', 'Aj4x1sBozz', 'qwerty123'];
+
+const isLoginValid = function (login) {
+  if (login.length >= 4 && login.length < 16) return true;
+  return false;
+};
+
+const isLoginUnique = function (allLogins, login) {
+  return !allLogins.includes(login);
+};
+
+const addLogin = function (allLogins, login) {
+  if (isLoginValid(login)) {
+    if (isLoginUnique(allLogins, login)) {
+      logins.push(login);
+      return 'Логин успешно добавлен!';
+    }
+    return 'Такой логин уже используется!';
+  }
+  return 'Ошибка! Логин должен быть от 4 до 16 символов';
+};
+
+// // Вызовы функции для проверки
+console.log(addLogin(logins, 'Ajax')); // 'Логин успешно добавлен!'
+console.log(addLogin(logins, 'robotGoogles')); // 'Такой логин уже используется!'
+console.log(addLogin(logins, 'Zod')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
+console.log(addLogin(logins, 'jqueryisextremelyfast')); // 'Ошибка! Логин должен быть от 4 до 16 символов'
